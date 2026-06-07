85072c1004647b8e5271abeccf0e1a6e73238b0c
JALmke <jallenludtke@gmail.com>
2026-06-07 15:33:44 -0500

    Delete src directory

--- src/main.jsx
+++
@@ -1,6 +0,0 @@
-// App entry point. Mounts the React app and loads global styles.
-import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import './styles.css';
-
-createRoot(document.getElementById('root')).render(<App />);
